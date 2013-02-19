a046dd9d1629076a88d26e7113f537cd98a2efc3
Warren Falk <warren@warrenfalk.com>
2013-02-19 17:42:18 -0500

    check correctness of typed character

--- src/warrenfalk/typegame/TypeGame.java
+++ src/warrenfalk/typegame/TypeGame.java
@@ -57,8 +57,12 @@ public class TypeGame {
 			// process input
 			while (Keyboard.next()) {
 				if (Keyboard.getEventKeyState()) {
-					nextChar++;
-					cursorPosition = calculateCursorPosition(font, challengeText, nextChar);
+					char kchar = Character.toLowerCase(Keyboard.getEventCharacter());
+					char cchar = Character.toLowerCase(challengeText.charAt(nextChar));
+					if (kchar == cchar) {
+						nextChar++;
+						cursorPosition = calculateCursorPosition(font, challengeText, nextChar);
+					}
 				}
 			}
 			
